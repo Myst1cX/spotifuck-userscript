f9b09c0a09fd3c98d479191c375e18b867955642
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2026-07-13 07:32:10 +0200

    Create spotifuck-v6.user.js

---
+++ spotifuck-v6.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Outdated Spotifuck - Link below
+// @namespace    https://github.com/Myst1cX/spotifuck-userscript
+// @version      7.0
+// @description  New install link: https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-mobile.user.js
+// @author       Myst1cX (adapted from Spotifuck app)
+// @match        *://open.spotify.com/*
+// @match        https://www.spotify.com/*/account/*
+// @match        https://www.spotify.com/*/premium/*
+// @match        https://www.spotify.com/*/duo/*
+// @match        https://www.spotify.com/*/student/*
+// @match        https://www.spotify.com/*/family/*
+// @match        https://payments.spotify.com/*
+// @grant        GM_addStyle
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @run-at       document-start
+// @homepageURL  https://github.com/Myst1cX/spotifuck-userscript
+// @supportURL   https://github.com/Myst1cX/spotifuck-userscript/issues
+// @updateURL    https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-v6.user.js
+// @downloadURL  https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-v6.user.js
+// ==/UserScript==
+
+// NEW LINK FROM NOW ON: https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-mobile.user.js
