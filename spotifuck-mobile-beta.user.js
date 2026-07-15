93d1c436da61308a1338ebdf418686f5c49d1262
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2026-07-16 00:43:28 +0200

    Update spotifuck-mobile-beta.user.js

--- spotifuck-mobile-beta.user.js
+++ spotifuck-mobile-beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spotifuck Mobile Beta
 // @namespace    https://github.com/Myst1cX/spotifuck-userscript
-// @version      7.9.beta
+// @version      8.0.maybe
 // @description  Full Spotifuck 1.6.4 UI hack (with minor tweaks) + playback control + force English UI + visual premium spoof
 // @author       Myst1cX (adapted from Spotifuck app)
 // @match        *://open.spotify.com/*
