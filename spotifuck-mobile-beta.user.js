1d92622e920503bf8f93dcc22144c8291383ef38
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2026-07-16 02:35:49 +0200

    Update spotifuck-mobile-beta.user.js

--- spotifuck-mobile-beta.user.js
+++ spotifuck-mobile-beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Spotifuck Mobile
+// @name         Spotifuck Mobile Beta
 // @namespace    https://github.com/Myst1cX/spotifuck-userscript
-// @version      7.5
+// @version      7.5.beta (ignore, same as stable version)
 // @description  Full Spotifuck 1.6.4 UI hack (with minor tweaks) + playback control + force English UI + visual premium spoof
 // @author       Myst1cX (adapted from Spotifuck app)
 // @match        *://open.spotify.com/*
@@ -18,8 +18,8 @@
 // @run-at       document-start
 // @homepageURL  https://github.com/Myst1cX/spotifuck-userscript
 // @supportURL   https://github.com/Myst1cX/spotifuck-userscript/issues
-// @updateURL    https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-mobile.user.js
-// @downloadURL  https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-mobile.user.js
+// @updateURL    https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-mobile-beta.user.js
+// @downloadURL  https://raw.githubusercontent.com/Myst1cX/spotifuck-userscript/main/spotifuck-mobile-beta.user.js
 // ==/UserScript==
 
 /*
